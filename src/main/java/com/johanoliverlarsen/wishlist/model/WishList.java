228069f3bf71eacf93895cd8258c946564d578f3
johanlarsen <johanoliverlarsen@gmail.com>
2026-04-14 14:43:02 +0200

    tilføjelse af Wishlist + Wishlistrepository med rowmapper + findbyId()

--- src/main/java/com/johanoliverlarsen/wishlist/model/WishList.java
+++ src/main/java/com/johanoliverlarsen/wishlist/model/WishList.java
@@ -5,8 +5,22 @@ public class WishList {
     private String title;
     private String description;
 
-    public WishList() {
-
+    public WishList(int wishListId, String title, String description) {
+        this.wishListId = wishListId;
+        this.title = title;
+        this.description = description;
     }
 
+    public int getWishListId() { return wishListId; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+
+    public void setWishListId(int wishListId) { this.wishListId = wishListId; }
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+
+    @Override
+    public String toString(){
+        return "WishListId: " + wishListId + ", Title: " + title + ", Description: " + description;
+    }
 }
--- src/main/java/com/johanoliverlarsen/wishlist/repository/WishListRepository.java
+++ src/main/java/com/johanoliverlarsen/wishlist/repository/WishListRepository.java
@@ -1,4 +1,54 @@
 package com.johanoliverlarsen.wishlist.repository;
 
+
+import com.johanoliverlarsen.wishlist.model.Wish;
+import com.johanoliverlarsen.wishlist.model.WishList;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class WishListRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private RowMapper<WishList> wishListRowMapper = (rs, rowNum) ->
+            new WishList(
+                    rs.getInt("wishlist_id"),
+                    rs.getString("title"),
+                    rs.getString("description")
+            );
+
+    public WishListRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+//    public List<WishList> findAll(){
+//    }
+
+    public WishList findById(int id){
+        String sql = """
+                SELECT w.wishlist_id, w.title, w.description
+                FROM wishlist w
+                WHERE w.wishlist_id = ?
+                """;
+        return jdbcTemplate.queryForObject(sql, wishListRowMapper, id);
+    }
+
+//    public List <WishList> findAllByProfileId(int profileId){
+//    }
+//
+//    public WishList insert(WishList wishlist){
+//    }
+//
+//    public boolean update(WishList wishlist){
+//    }
+//
+//    public boolean deleteById(int wishListId){
+//    }
+//
+
+
+
+
 }
