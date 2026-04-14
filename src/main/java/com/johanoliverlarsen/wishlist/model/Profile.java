908627d7ea3fcb4887daeb9c5df09def0f9d53ee
anph1000 <anph1000@stud.ek.dk>
2026-04-14 14:06:38 +0200

    first commit

--- src/main/java/com/johanoliverlarsen/wishlist/model/Profile.java
+++ src/main/java/com/johanoliverlarsen/wishlist/model/Profile.java
@@ -1,4 +1,59 @@
 package com.johanoliverlarsen.wishlist.model;
 
 public class Profile {
+    private int profileId;
+    private String name;
+    private String email;
+    private String password;
+
+    public Profile(int profileId, String name, String email, String password) {
+        this.profileId = profileId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public int getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(int profileId) {
+        this.profileId = profileId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "profileId=" + profileId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
+
