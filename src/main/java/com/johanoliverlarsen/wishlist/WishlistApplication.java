2c26f00c7747d28eb9d3d7288fb77883044ea37d
Johan Larsen <johanoliverlarsen@gmail.com>
2026-04-17 12:57:05 +0200

    Revert "50-wishrepositorytest"

--- src/main/java/com/johanoliverlarsen/wishlist/WishlistApplication.java
+++ src/main/java/com/johanoliverlarsen/wishlist/WishlistApplication.java
@@ -1,6 +1,7 @@
 package com.johanoliverlarsen.wishlist;
 
 import com.johanoliverlarsen.wishlist.repository.WishRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,14 @@ public class WishlistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WishlistApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner testDb(WishRepository repo) {
+        return args -> {
+            System.out.println("=== TEST: findTagsById() ===");
+            System.out.println(repo.findById(2));
+        };
     }
 
 }
